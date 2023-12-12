fb17dce81adbd27b769aba48cd4abea99174e5ef
ltpoole <laborris.poole@gmail.com>
2023-12-12 07:33:04 -0500

    verified data appeared in console log in App component.

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,21 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 function App() {
+  const [Songs, setSongs] = useState([]);
+
+  const displaySongs = async () => {
+    try {
+      const response = await axios.get("https://localhost:7081/api/Songs");
+      console.log(response);
+    } catch (error) {
+      console.warn("Error in displaySongs request: ", error);
+    }
+  };
+
+  useEffect(() => {
+    displaySongs();
+  }, []);
+
   return (
     <div className="App">
       <Header />
