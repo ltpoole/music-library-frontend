d7c926cf66545623147be2f90f07d7204ca307e9
ltpoole <laborris.poole@gmail.com>
2023-12-12 00:11:21 -0500

    created and rendered MusicTable component in App

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import "./App.css";
 import Header from "./components/Header/Header";
 import SearchBar from "./components/SearchBar/SearchBar";
-import React from "react";
+import MusicTable from "./components/MusicTable/MusicTable";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
     <div className="App">
       <Header />
       <SearchBar />
+      <MusicTable />
     </div>
   );
 }
--- src/components/MusicTable/MusicTable.jsx
+++ src/components/MusicTable/MusicTable.jsx
@@ -1,5 +1,15 @@
 const MusicTable = ({}) => {
-  return <div></div>;
+  return (
+    <div>
+      <span>Title</span>
+      <span>Artist</span>
+      <span>Album</span>
+      <span>Genre</span>
+      <span>Release Date</span>
+      <span></span>
+      <span></span>
+    </div>
+  );
 };
 
 export default MusicTable;
