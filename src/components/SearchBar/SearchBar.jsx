bacd9bae5301569410b41a43efbfe16b908f3f33
ltpoole <laborris.poole@gmail.com>
2023-12-11 23:30:00 -0500

    created and rendered SearchBar in App

---
+++ src/components/SearchBar/SearchBar.jsx
@@ -0,0 +1,12 @@
+const SearchBar = ({}) => {
+  return (
+    <form>
+      <div>
+        <label>Search:</label>
+        <input type="text" />
+      </div>
+    </form>
+  );
+};
+
+export default SearchBar;
