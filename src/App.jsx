f4bd305ab896feb411566ca1b31e386cba8f4e44
ltpoole <laborris.poole@gmail.com>
2023-12-11 23:37:19 -0500

    imported react and axios in App

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import "./App.css";
 import Header from "./components/Header/Header";
 import SearchBar from "./components/SearchBar/SearchBar";
+import React from "react";
+import axios from "axios";
 
 function App() {
   return (
